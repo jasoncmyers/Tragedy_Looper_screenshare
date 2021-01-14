ec2035599fce4a5156a5dad4d45b1e6faba3790a
Jason Myers <myers.jason.c@gmail.com>
2021-01-14 17:23:18 -0600

    Beging card and incident board logic scripts

---
+++ Assets/Scripts/CharacterCardLogic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCardLogic : MonoBehaviour
+{
+    public CharacterCardData cardData;
+    public int paranoia;
+    public int goodwill;
+    public bool dead;
+
+
+    private void OnMouseOver()
+    {
+        Debug.Log("MouseOver on card " + cardData?.characterName);
+    }
+
+
+    private void Update()
+    {
+        if (dead && transform.eulerAngles == Vector3.zero)
+        {
+            Vector3 temp = new Vector3(0, 0, -90);
+            transform.eulerAngles = temp;
+        }
+        else if (!dead)
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/IncidentBoardLogic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IncidentBoardLogic : MonoBehaviour
+{
+
+    public Sprite loopMarker;
+    public Sprite dayMarker;
+    public Sprite incidentMarker;
+    public Sprite extraMarker;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
