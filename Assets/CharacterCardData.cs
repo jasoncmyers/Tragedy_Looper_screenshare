8e7e3c5abc133e72841e8242f31c48ca52c8ff23
Jason Myers <myers.jason.c@gmail.com>
2021-01-14 16:09:33 -0600

    Add CharacterCardData scriptable object

---
+++ Assets/CharacterCardData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CharacterCardData", menuName = "Character Card Data", order = 51)]
+public class CharacterCardData : ScriptableObject
+{
+    public string characterName;
+    public Sprite characterPicture;
+    public int paranoiaLimit;
+    public List<int> goodWillAbilityLevels;
+
+    public List<int> forbiddenLocations;
+}
