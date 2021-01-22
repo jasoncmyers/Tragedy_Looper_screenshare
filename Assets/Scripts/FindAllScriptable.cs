b75d51041d36560ba0941d28cf7c73e752fb5e44
Jason Myers <myers.jason.c@gmail.com>
2021-01-21 21:09:14 -0600

    First attempt to automate finding of all CharacterCardData scriptable objects

---
+++ Assets/Scripts/FindAllScriptable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindAllScriptable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        CharacterCardData[] allCards = Resources.FindObjectsOfTypeAll<CharacterCardData>();
+
+
+        //CharacterCardData[] allCards = FindObjectsOfType(typeof(CharacterCardData)) as CharacterCardData[];
+
+        Debug.Log("All " + allCards.Length + " cards found:");
+        foreach (var c in allCards)
+        {
+            Debug.Log(c.characterName);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
