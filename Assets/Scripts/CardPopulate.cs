f728448867305fc8ace29ec6abcc48f6c699f295
Jason Myers <myers.jason.c@gmail.com>
2021-01-25 13:16:26 -0600

    Made some demo scripts to work on menu pop-ups

---
+++ Assets/Scripts/CardPopulate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardPopulate : MonoBehaviour
+{
+
+    GameObject MainPanel;
+    public CharacterCardData[] availableCharacterCards;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainPanel = this.gameObject;
+        foreach (var card in availableCharacterCards)
+        {
+            Button temp = MainPanel.AddComponent<Button>();
+            //temp.
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ItemController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class ItemController : MonoBehaviour
+{
+
+    public Button sampleButton;
+    private List<ContextMenuItem> contextMenuItems;
+
+    private void Awake()
+    {
+        contextMenuItems = new List<ContextMenuItem>();
+        Action<Image> equip = new Action<Image>(EquipAction);
+        Action<Image> use = new Action<Image>(UseAction);
+        Action<Image> drop = new Action<Image>(DropAction);
+
+        contextMenuItems.Add(new ContextMenuItem("Equip", sampleButton, equip));
+        contextMenuItems.Add(new ContextMenuItem("Use", sampleButton, use));
+        contextMenuItems.Add(new ContextMenuItem("Drop", sampleButton, drop));
+    }
+
+    private void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Activating menu!");
+
+            Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+            ContextMenu.Instance.CreateContextMenu(contextMenuItems, new Vector2(pos.x, pos.y));
+        }
+    }
+
+    void EquipAction(Image contextPanel)
+    {
+        Debug.Log("Equipped");
+        Destroy(contextPanel.gameObject);
+    }
+
+    void UseAction(Image contextPanel)
+    {
+        Debug.Log("Used");
+        Destroy(contextPanel.gameObject);
+    }
+
+    void DropAction(Image contextPanel)
+    {
+        Debug.Log("Dropped");
+        Destroy(contextPanel.gameObject);
+    }
+}
