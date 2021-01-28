4d1b33df4c81d1558773f9ab480932db4f7608a3
Jason Myers <myers.jason.c@gmail.com>
2021-01-28 09:28:52 -0600

    Add camera scaler to deal with non-16:9 screen aspects

---
+++ Assets/Scripts/CameraScaler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraScaler : MonoBehaviour
+{
+    Camera cam;
+    public float desiredAspectRatio = 16f / 9f;
+    private float lastGoodAspect;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = this.GetComponent<Camera>();
+        lastGoodAspect = desiredAspectRatio;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cam.aspect != lastGoodAspect)
+        {
+            float new_ratio = desiredAspectRatio / cam.aspect;
+            cam.orthographicSize *= new_ratio;
+            lastGoodAspect = cam.aspect;
+        }
+    }
+}
