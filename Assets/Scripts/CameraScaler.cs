883aca16775c1758f3f3951839e1cd5927e6f117
Jason Myers <myers.jason.c@gmail.com>
2021-01-28 22:18:22 -0600

    Fix bug with CameraScaler logic

--- Assets/Scripts/CameraScaler.cs
+++ Assets/Scripts/CameraScaler.cs
@@ -6,12 +6,16 @@ public class CameraScaler : MonoBehaviour
     Camera cam;
     public float desiredAspectRatio = 16f / 9f;
     private float lastGoodAspect;
+    private float heightPx;
+    private float heightOrtho;
     
     // Start is called before the first frame update
     void Start()
     {
         cam = this.GetComponent<Camera>();
         lastGoodAspect = desiredAspectRatio;
+        heightOrtho = cam.orthographicSize;
+        heightPx = Screen.height;
     }
 
     // Update is called once per frame
@@ -19,9 +23,25 @@ public class CameraScaler : MonoBehaviour
     {
         if (cam.aspect != lastGoodAspect)
         {
-            float new_ratio = desiredAspectRatio / cam.aspect;
-            cam.orthographicSize *= new_ratio;
-            lastGoodAspect = cam.aspect;
+            NewScale();
         }
+    }
+
+    void NewScale()
+    {
+        
+        float screenAspect = (float)Screen.width / (float)Screen.height;
+        if (screenAspect < desiredAspectRatio)
+        {
+            // the screen isn't wide enough, so reduce the height and letterbox it
+            cam.orthographicSize = heightOrtho * desiredAspectRatio / screenAspect;
+        }
+        else
+        {
+            // if wide enough, reset to the default and let the automated height scaling handle it
+            cam.orthographicSize = heightOrtho;
+        }
+
+        lastGoodAspect = cam.aspect;
     }
 }
